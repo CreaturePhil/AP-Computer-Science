34cb2dea5ce26212f01c839cecd6a29fe0519c9a
Phil <CreaturePhil@users.noreply.github.com>
2014-02-20 22:33:58 -0800

    Update Poem.java

--- Poem.java
+++ Poem.java
@@ -2,13 +2,13 @@ public class Poem {
 
 	public static void main(String[] args) {
 	
-	  String one = " Roses are red, \n Violets are blue, ";
-	  
-	  String two = " \n Sugar is sweet,";
-	  
-	  String three = "\n But I have \"commitment issues\", ";
-	  
-	  System.out.println(one + two + three + "\n So I'd rather just be friends \n At this point in our relationship");
+		String one = " Roses are red, \n Violets are blue, ";
+		
+		String two = " \n Sugar is sweet,";
+		
+		String three = "\n But I have \"commitment issues\", ";
+		
+		System.out.println(one + two + three + "\n So I'd rather just be friends \n At this point in our relationship");
 	
 	}
 
