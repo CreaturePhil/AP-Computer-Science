eb9eedfe7c8735ef8b9e617c5d4616d9a855b5fa
Phil <CreaturePhil@users.noreply.github.com>
2014-02-20 22:32:23 -0800

    Create javamath.java

---
+++ javamath.java
@@ -0,0 +1,29 @@
+public class Wat { 
+ 
+  public static void main(String[] args) { 
+    
+    int num1, num2; 
+    int add, sub, multi, div, modulus; 
+    num1 = 10; 
+    num2 = 10; 
+     
+    add = num1 + num2; 
+    sub = num1 - num2; 
+    multi = num1*num2; 
+    div = num1/num2; 
+    modulus = num1%num2; 
+     
+    System.out.println("The addition of " + num1 + " and " + num2 + " is " + add); 
+    System.out.println("The subtraction of " + num1 + " and " + num2 + " is " + sub); 
+    System.out.println("The multiplication of " + num1 + " and " + num2 + " is " + multi); 
+    System.out.println("The division of " + num1 + " and " + num2 + " is " + div); 
+    System.out.println("The modulus of " + num1 + " and " + num2 + " is " + modulus); 
+   
+   
+       
+   
+  } 
+ 
+} 
+ 
+ 
