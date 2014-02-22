14922bbace614ccb2ad38f69ba54efb7e7645d7e
Phil <CreaturePhil@users.noreply.github.com>
2014-02-21 18:14:10 -0800

    Update MirrorHorizontal.java

--- MirrorHorizontal.java
+++ MirrorHorizontal.java
@@ -1,10 +1,10 @@
 public void mirrorHorizontal(){ 
-  Pixel[][] pixels = this.getPixels2D(); 
-  for (int column = 0; column < pixels[0].length; column++){ 
-	  for(int row = 0; row < pixels.length; row++){ 
-		  Pixel point = pixels[row][column]; 
-		  pixels[pixels.length-1-row][column].setColor(point.getColor()); 
-	  } 
+	Pixel[][] pixels = this.getPixels2D(); 
+	for (int column = 0; column < pixels[0].length; column++){ 
+		for(int row = 0; row < pixels.length; row++){ 
+			Pixel point = pixels[row][column]; 
+			pixels[pixels.length-1-row][column].setColor(point.getColor()); 
+		} 
 	}  
 } 
    
