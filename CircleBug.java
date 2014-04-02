5d8374a01e73da5a378302b2a7ce8e3889c92ac2
Phil <CreaturePhil@users.noreply.github.com>
2014-04-02 14:15:57 -0700

    Update CircleBug.java

--- CircleBug.java
+++ CircleBug.java
@@ -1,22 +1,24 @@
 import info.gridworld.actor.Bug;
 
 public class CircleBug extends Bug{
-    private int steps, sideLength;
-    public CircleBug(int length){
-        steps = 0;
-        sideLength = length;
-    }
-    public void act(){
-        if (steps &lt; sideLength && canMove())
-        {
-            move();
-            steps++;
-        }
-        else
-        {
-            turn();
-            steps = 0;
-        }
-        
-    }
+	private int steps, sideLength;
+	public CircleBug(int length){
+		steps = 0;
+		sideLength = length;
+	}
+	public void act(){
+		if (steps < sideLength && canMove()) 
+		 { 
+			move(); 
+			steps++; 
+		 } 
+		 else 
+		 { 
+			turn(); 
+			steps = 0; 
+		 }
+		
+	}
 }
+
+
