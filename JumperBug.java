1e9c172b5a02cf0235b5933417646aaaff8ffac9
Phil <CreaturePhil@users.noreply.github.com>
2014-04-02 14:06:27 -0700

    Create JumperBug.java

---
+++ JumperBug.java
@@ -0,0 +1,57 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Bug;
+import info.gridworld.actor.Flower;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+public class JumperBug extends Bug{
+    private int steps, sideLength;
+    
+    public JumperBug(int length){
+        steps = 0;
+        sideLength = length;
+    }
+    
+    public void move() {
+        Grid&lt;Actor&gt; gr = getGrid();
+        if (gr == null)
+        return;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        Location jump = next.getAdjacentLocation(getDirection());
+        if (gr.isValid(jump)) {
+            moveTo(jump);
+        }else
+        removeSelfFromGrid();
+        Flower flower = new Flower(getColor());
+        flower.putSelfInGrid(gr, loc);
+    }
+    
+    public boolean canMove() {
+        Grid&lt;Actor&gt; gr = getGrid();
+        if (gr == null)
+        return false;
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        Location jump = next.getAdjacentLocation(getDirection());
+        if (!gr.isValid(jump))
+        return false;
+        Actor neighbor = gr.get(next);
+        return (neighbor == null) || (neighbor instanceof Flower);
+        // ok to move into empty location or onto flower
+        // not ok to move onto any other actor
+    }
+    
+    
+    
+    
+    public void act(){
+        if (steps &lt; sideLength && canMove()){
+            move();
+            steps++;
+            } else {
+            steps = 0;
+        }
+    }
+}
+ 
