2bf0c3348d8ec06e056922a44f501ae37143aa98
Phil <CreaturePhil@users.noreply.github.com>
2014-04-02 14:08:43 -0700

    Create SmellyCritter.java

---
+++ SmellyCritter.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import info.gridworld.actor.Critter;
+import info.gridworld.actor.Flower;
+import info.gridworld.actor.Rock;
+import info.gridworld.actor.Actor;
+import info.gridworld.actor.Bug;
+import info.gridworld.grid.Location;
+
+public class SmellyCritter extends Critter{
+    
+    private int sf;
+    
+    public SmellyCritter(int smellFactor){
+        sf = smellFactor;
+    }
+    
+    public SmellyCritter(){
+        sf = 2;
+    }
+    
+    public ArrayList&lt;Actor&gt; getActors(){
+        ArrayList&lt;Actor&gt; actors = new ArrayList&lt;Actor&gt;();
+        Location loc = getLocation();
+        int row = loc.getRow();
+        int col = loc.getCol();
+        for (int r = row-sf; r &lt;= row+sf; r++){
+            for (int c = col-sf; c &lt;= col+sf; c++){
+                Location temp = new Location(r,c);
+                if (getGrid().isValid(temp)) {
+                    Actor a = getGrid().get(temp);
+                    if (a != null && a != this) {
+                        actors.add(a);
+                    }
+                }
+            }
+        }
+        return actors;
+    }
+    
+    public void processActors(ArrayList&lt;Actor&gt; actors) {
+        for (Actor a : actors) {
+            a.setDirection(a.getLocation().getDirectionToward(getLocation())+Location.HALF_CIRCLE);
+            /*
+            Location smellyCritterLoc = getLocation();
+            Location actorLoc = a.getLocation();
+            int directionOfSmellyCritter = actorLoc.getDirectionToward(smellyCritterLoc);
+            int turn180 = Location.HALF_CIRCLE;
+            int turningDirection = directionOfSmellyCritter+turn180;
+            a.setDirection(turningDirection);
+            */
+        }
+    }
+    
+}
