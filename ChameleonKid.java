cab45812f46b2ccee7f342e9315ae896a01edbfa
Phil <CreaturePhil@users.noreply.github.com>
2014-04-02 14:07:24 -0700

    Create ChameleonKid.java

---
+++ ChameleonKid.java
@@ -0,0 +1,35 @@
+import info.gridworld.actor.Actor;
+import info.gridworld.grid.Grid;
+import info.gridworld.grid.Location;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ChameleonKid extends ChameleonCritter{
+    public void processActors(ArrayList&lt;Actor&gt; actors){
+        if(actors.size()  == 0){
+            return;
+        }
+        
+        Grid&lt;Actor&gt; gr = getGrid();
+        Location loc = getLocation();
+        Location next = loc.getAdjacentLocation(getDirection());
+        Location nextLeft = loc.getAdjacentLocation(getDirection()-45);
+        Location nextRight = loc.getAdjacentLocation(getDirection()+45);
+        Actor neighborFront = gr.get(next);
+        Actor neighborFrontLeft = gr.get(nextLeft);
+        Actor neighborFrontRight = gr.get(nextRight);
+        
+        if (neighborFront != null){
+            setColor(neighborFront.getColor());
+        }
+        else if (neighborFrontLeft != null){
+            setColor(neighborFrontLeft.getColor());
+        }
+        else if (neighborFrontRight != null){
+            setColor(neighborFrontRight.getColor());
+        }
+    }
+    
+}
