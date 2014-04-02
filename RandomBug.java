b237a2be6e2e3b5a69170cc7953948c493f80ccc
Phil <CreaturePhil@users.noreply.github.com>
2014-04-02 14:04:48 -0700

    Create RandomBug.java

---
+++ RandomBug.java
@@ -0,0 +1,26 @@
+import java.util.Random;
+import info.gridworld.actor.Bug;
+
+public class RandomBug extends Bug{
+    private int steps, length;
+    Random rng = new Random();
+    
+    public RandomBug(){
+        steps = 0;
+        length = rng.nextInt(10);
+    }
+    public void turn(){
+        setDirection(getDirection() + rng.nextInt(360));
+    }
+    
+    public void act(){
+        if (steps &lt; length && canMove()){
+            move();
+            steps++;
+            } else {
+            turn();
+            steps = 0;
+            length = rng.nextInt(10);
+        }
+    }
+}
