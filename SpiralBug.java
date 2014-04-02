976da2f7ee99be83a9659a4864705f0b8047db21
Phil <CreaturePhil@users.noreply.github.com>
2014-04-02 14:13:49 -0700

    Update SpiralBug.java

--- SpiralBug.java
+++ SpiralBug.java
@@ -2,24 +2,28 @@ import info.gridworld.actor.Bug;
 import info.gridworld.actor.Bug;
 
 public class SpiralBug extends Bug{
-    
-    private int steps, sideLength;
-    public SpiralBug(int length){
-        steps = 0;
-        sideLength = length;
-    }
-    public void act(){
-        if (steps &lt; sideLength && canMove())
-        {
-            move();
-            steps++;
-        }
-        else
-        {
-            turn();
-            steps = 0;
-            sideLength++;
-        }
-        
-    }
+
+	private int steps, sideLength;
+	public SpiralBug(int length){
+		steps = 0;
+		sideLength = length;
+	}
+	public void act(){
+		if (steps < sideLength && canMove()) 
+		{ 
+			move(); 
+			steps++;
+		} 
+		else 
+		{ 
+			turn();
+			steps = 0;
+			sideLength++;
+		}
+			
+	}
 }
+
+
+
+
