4157076727d17b62853f029ab4eeae03c36faf63
Phil <CreaturePhil@users.noreply.github.com>
2014-04-02 14:18:39 -0700

    Update ChameleonKid.java

--- ChameleonKid.java
+++ ChameleonKid.java
@@ -1,35 +1,36 @@
 import info.gridworld.actor.Actor;
 import info.gridworld.grid.Grid;
 import info.gridworld.grid.Location;
-
+ 
 import java.util.ArrayList;
 import java.util.List;
-
-
+ 
+ 
 public class ChameleonKid extends ChameleonCritter{
-    public void processActors(ArrayList&lt;Actor&gt; actors){
+    public void processActors(ArrayList<Actor> actors){
         if(actors.size()  == 0){
             return;
         }
         
-        Grid&lt;Actor&gt; gr = getGrid();
-        Location loc = getLocation();
-        Location next = loc.getAdjacentLocation(getDirection());
-        Location nextLeft = loc.getAdjacentLocation(getDirection()-45);
-        Location nextRight = loc.getAdjacentLocation(getDirection()+45);
+        Grid<Actor> gr = getGrid();
+        Location loc = getLocation(); 
+        Location next = loc.getAdjacentLocation(getDirection()); 
+        Location nextLeft = loc.getAdjacentLocation(getDirection()-45); 
+        Location nextRight = loc.getAdjacentLocation(getDirection()+45); 
         Actor neighborFront = gr.get(next);
         Actor neighborFrontLeft = gr.get(nextLeft);
         Actor neighborFrontRight = gr.get(nextRight);
         
         if (neighborFront != null){
-            setColor(neighborFront.getColor());
+            setColor(neighborFront.getColor()); 
         }
         else if (neighborFrontLeft != null){
-            setColor(neighborFrontLeft.getColor());
+            setColor(neighborFrontLeft.getColor()); 
         }
         else if (neighborFrontRight != null){
-            setColor(neighborFrontRight.getColor());
+            setColor(neighborFrontRight.getColor()); 
         }
     }
-    
+ 
 }
+
